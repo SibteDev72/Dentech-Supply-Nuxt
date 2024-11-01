e396b1617e4e9efa8eb32d6d7c814e13c962ef4a
Sibte Muhammad <sibte.dev72@gmail.com>
2024-11-01 18:40:09 +0500

    Working on Product Page

--- pages/Product/[productId].vue
+++ pages/Product/[productId].vue
@@ -1,14 +1,35 @@
 <template>
   <div
-    class="mt-[96px] lg:mt-[192px] w-full min-h-[calc(100vh-96px)] lg:min-h-[calc(100vh-192px)] flex flex-col justify-center items-center"
+    v-if="productData"
+    class="mt-[96px] lg:mt-[192px] px-4 w-full h-fit py-[5rem] flex flex-col lg:flex-row gap-4"
   >
-    <p v-if="productData" class="text-lg font-extrabold">
-      {{ productData.title }}
-    </p>
-    <p v-if="productData" class="text-sm font-bold">id: {{ productData.id }}</p>
-    <p v-if="productData" class="text-sm font-bold">
-      Price: {{ productData.price }}Rs
-    </p>
+    <div class="w-full lg:w-[30%] flex flex-col gap-8">
+      <img :src="`${currentProductImg.src}`" class="self-center w-full" />
+      <div class="hidden w-full lg:flex flex-row justify-center gap-2">
+        <img
+          @click="
+            () => {
+              currentProductImg = img;
+            }
+          "
+          v-for="(img, index) in productData.imgSrc"
+          :src="`${img.src}`"
+          :key="index"
+          :class="`w-[100px] shadow-sm cursor-pointer ${
+            img.id === currentProductImg.id ? 'shadow-bgColor3' : 'shadow-black'
+          }`"
+        />
+      </div>
+    </div>
+    <div class="w-full lg:w-[70%] flex flex-col gap-2">
+      <p class="text-lg lg:text-xl font-extrabold">{{ productData.title }}</p>
+      <p class="text-sm lg:text-lg text-textColor4 font-extrabold">
+        Rs{{ productData.price }}.00
+      </p>
+      <p class="text-sm lg:text-lg font-bold">
+        Description: {{ productData.description }}
+      </p>
+    </div>
   </div>
 </template>
 
@@ -18,12 +39,15 @@ import type { ProductItem } from "~/types/Products";
 import { getProductDetails } from "~/API/Products";
 
 const productData = ref<ProductItem>();
+const currentProductImg = ref();
 
 const fetchProductDetails = async (productID: number) => {
   const data = await getProductDetails(productID);
   if (data) {
     //@ts-ignore
     productData.value = data;
+    //@ts-ignore
+    currentProductImg.value = productData.value.imgSrc[0];
   }
 };
 
